04e4b9f9afc5c6fc30dc5f0ea0859124ef16cbef
byf3332 <71202267+byf3332@users.noreply.github.com>
2026-07-27 23:04:31 +0800

    Fix base product inheritance for recovery ramdisk

--- twrp_P720S20.mk
+++ twrp_P720S20.mk
@@ -8,6 +8,9 @@
 # 64-bit recovery userspace.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
+# Base AOSP product. Required to populate TARGET_ROOT_OUT used by recovery packaging.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
 # Android devices without a dedicated recovery partition use the generic
 # ramdisk model and place recovery resources in vendor_boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
