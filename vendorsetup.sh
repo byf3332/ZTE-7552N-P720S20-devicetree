b25e8a445484a61de41822aaab2aef6acb09eaa8
byf3332 <71202267+byf3332@users.noreply.github.com>
2026-08-10 21:11:53 +0800

    P720S20: restore metadata FBE state for password decrypt

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -30,6 +30,8 @@ python3 "${_p720s20_tree_dir}/tools/install_p720s20_keystore2_fix.py" \
     "${_p720s20_android_top}/system/vold/KeyStorage.cpp" \
     "${_p720s20_android_top}/system/vold/MetadataCrypt.cpp" \
     "${_p720s20_android_top}/system/vold/KeyUtil.cpp" || return 1
+python3 "${_p720s20_tree_dir}/tools/install_p720s20_metadata_fbe_remount.py" \
+    "${_p720s20_android_top}/bootable/recovery/partitionmanager.cpp" || return 1
 python3 "${_p720s20_tree_dir}/tools/install_p720s20_adb_reboot.py" \
     "${_p720s20_android_top}/system/core/reboot/reboot.c" || return 1
 unset _p720s20_android_top _p720s20_tree_dir
