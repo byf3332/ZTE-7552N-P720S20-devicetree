efa4b01b0aa9546462b887f09ac35ce08970445f
byf3332 <71202267+byf3332@users.noreply.github.com>
2026-08-11 19:37:45 +0800

    P720S20: fix FunctionFS MTP gadget

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -29,6 +29,8 @@ patch -d "${_p720s20_android_top}/bootable/recovery" -p1 \
     < "${_p720s20_tree_dir}/patches/hide_unsupported_advanced_actions.patch" || return 1
 patch -d "${_p720s20_android_top}/bootable/recovery" -p1 \
     < "${_p720s20_tree_dir}/patches/preserve_boot_fastboot_bcb.patch" || return 1
+patch -d "${_p720s20_android_top}/bootable/recovery" -p1 \
+    < "${_p720s20_tree_dir}/patches/unisoc_mtp_ffs_v1.patch" || return 1
 python3 "${_p720s20_tree_dir}/tools/install_generic_drm_backend.py" \
     "${_p720s20_tree_dir}/patches/graphics_drm.cpp" \
     "${_p720s20_android_top}/bootable/recovery/minuitwrp/graphics_drm.cpp" || return 1
