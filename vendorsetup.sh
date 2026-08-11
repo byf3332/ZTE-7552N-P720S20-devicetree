26a3397f2026aa8b627390a77e726ca97d82c8c7
byf3332 <71202267+byf3332@users.noreply.github.com>
2026-08-11 11:08:03 +0800

    P720S20: harden Gatekeeper FBE unlock

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -32,6 +32,8 @@ python3 "${_p720s20_tree_dir}/tools/install_p720s20_keystore2_fix.py" \
     "${_p720s20_android_top}/system/vold/KeyUtil.cpp" || return 1
 python3 "${_p720s20_tree_dir}/tools/install_p720s20_metadata_fbe_remount.py" \
     "${_p720s20_android_top}/bootable/recovery/partitionmanager.cpp" || return 1
+python3 "${_p720s20_tree_dir}/tools/install_p720s20_gatekeeper_token_guard.py" \
+    "${_p720s20_android_top}/system/vold/Decrypt.cpp" || return 1
 python3 "${_p720s20_tree_dir}/tools/install_p720s20_adb_reboot.py" \
     "${_p720s20_android_top}/system/core/reboot/reboot.c" || return 1
 unset _p720s20_android_top _p720s20_tree_dir
