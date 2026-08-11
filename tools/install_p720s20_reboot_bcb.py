ebf42b04a2646e5812ca5d96505fe0424e94168a
byf3332 <71202267+byf3332@users.noreply.github.com>
2026-08-11 21:33:32 +0800

    P720S20: follow stock fastbootd boot chain

--- tools/install_p720s20_reboot_bcb.py
+++ tools/install_p720s20_reboot_bcb.py
@@ -86,11 +86,11 @@ s = one(s, """\t\tcase rb_recovery:
 s = one(s, """\t\tcase rb_fastboot:
 \t\t\treturn property_set(ANDROID_RB_PROPERTY, "reboot,fastboot");
 """, """\t\tcase rb_fastboot:
-\t\t\tif (!WriteP720S20BootloaderMessage("", ""))
+\t\t\tif (!WriteP720S20BootloaderMessage("boot-fastboot", ""))
 \t\t\t\treturn -1;
 \t\t\treturn __reboot(LINUX_REBOOT_MAGIC1, LINUX_REBOOT_MAGIC2,
 \t\t\t                  LINUX_REBOOT_CMD_RESTART2,
-\t\t\t                  const_cast<char*>("fastboot"));
+\t\t\t                  const_cast<char*>("recovery"));
 """, "fastbootd reboot")
 
 target.write_text(s)
